6811bde3c34a902aa1be29e391d2bc5ff4454fa2
Daman <daman.fergus@justice.gov.uk>
2018-02-20 13:21:08 +0000

    Damans inital draft

--- app/routes.js
+++ app/routes.js
@@ -7,5 +7,80 @@ router.get('/', function (req, res) {
 })
 
 // Add your routes here - above the module.exports line
+router.get('/inital-page', function (req, res) {
+  res.render('initalpage')
+});
+
+// Branching
+
+// Branching
+router.get('/questiontwotest', function (req, res) {
+  // Get the answer from the query string (eg. ?over18=false)
+  var changedName = req.query.changedName
+
+  if (changedName === 'yes') {
+    // Redirect to the relevant page
+    res.redirect('/failure')
+  } else {
+    // If over18 is any other value (or is missing) render the page requested
+    res.render('questionthree')
+  }
+})
+
+// Branching
+router.get('/questionthreetest', function (req, res) {
+  // Get the answer from the query string (eg. ?over18=false)
+  var changedName = req.query.changedName
+
+  if (changedName === 'no') {
+    // Redirect to the relevant page
+    res.redirect('/failure')
+  } else {
+    // If over18 is any other value (or is missing) render the page requested
+    res.render('questionfour')
+  }
+})
+
+// Branching
+router.get('/questionfourtest', function (req, res) {
+  // Get the answer from the query string (eg. ?over18=false)
+  var changedName = req.query.changedName
+
+  if (changedName === 'no') {
+    // Redirect to the relevant page
+    res.redirect('/failure')
+  } else {
+    // If over18 is any other value (or is missing) render the page requested
+    res.render('questionfive')
+  }
+})
+
+// Branching
+router.get('/questionfivetest', function (req, res) {
+  // Get the answer from the query string (eg. ?over18=false)
+  var changedName = req.query.changedName
+
+  if (changedName === 'no') {
+    // Redirect to the relevant page
+    res.redirect('/failure')
+  } else {
+    // If over18 is any other value (or is missing) render the page requested
+    res.render('success')
+  }
+})
+
+// Branching
+router.get('/questiontwotest', function (req, res) {
+  // Get the answer from the query string (eg. ?over18=false)
+  var changedName = req.query.changedName
+
+  if (changedName === 'no') {
+    // Redirect to the relevant page
+    res.redirect('/failure')
+  } else {
+    // If over18 is any other value (or is missing) render the page requested
+    res.redirect('/questionthree')
+  }
+})
 
 module.exports = router
