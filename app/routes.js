c9c60cda7d8532f300197335d5cc46655bdb397e
Daman <daman.fergus@justice.gov.uk>
2018-03-14 14:30:21 +0000

    Added a loop that allows a user to select multiple banks. Fixed step counter

--- app/routes.js
+++ app/routes.js
@@ -19,4 +19,43 @@ router.get('/generic-start', function (req, res) {
   res.render('generic-start')
 });
 
+router.get('/generic/multibank', function (req, res) {
+  // Get the answer from the query string (eg. ?over18=false)
+  var multiBank = req.query.multibank
+
+  if (multiBank === 'yes') {
+    // Redirect to the relevant page
+    res.redirect('/generic/obbank')
+  } else {
+    // If over18 is any other value (or is missing) render the page requested
+    res.render('generic/benefits-kind')
+  }
+})
+
+router.get('/civil/multibank', function (req, res) {
+  // Get the answer from the query string (eg. ?over18=false)
+  var multiBank = req.query.multibank
+
+  if (multiBank === 'yes') {
+    // Redirect to the relevant page
+    res.redirect('/civil/obbank')
+  } else {
+    // If over18 is any other value (or is missing) render the page requested
+    res.render('civil/benefits-kind')
+  }
+})
+
+router.get('/crime/multibank', function (req, res) {
+  // Get the answer from the query string (eg. ?over18=false)
+  var multiBank = req.query.multibank
+
+  if (multiBank === 'yes') {
+    // Redirect to the relevant page
+    res.redirect('/crime/obbank')
+  } else {
+    // If over18 is any other value (or is missing) render the page requested
+    res.render('crime/benefits-kind')
+  }
+})
+
 module.exports = router
