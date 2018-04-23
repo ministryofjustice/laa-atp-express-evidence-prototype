18881e807abc4adcc876f0372b8a1c7474189bd0
Mark Whitaker <mark.whitaker@justice.gov.uk>
2018-04-23 14:00:18 +0100

    Create new citizen-truelayer prototype for new flow

--- app/routes.js
+++ app/routes.js
@@ -47,6 +47,19 @@ router.get('/generic-truelayer/multibank', function (req, res) {
   }
 })
 
+router.get('/citizen-truelayer/multibank', function (req, res) {
+  // Get the answer from the query string (eg. ?over18=false)
+  var multiBank = req.query.multibank
+
+  if (multiBank === 'yes') {
+    // Redirect to the relevant page
+    res.redirect('/citizen-truelayer/obbank')
+  } else {
+    // If over18 is any other value (or is missing) render the page requested
+    res.render('citizen-truelayer/other-income')
+  }
+})
+
 router.get('/civil/multibank', function (req, res) {
   // Get the answer from the query string (eg. ?over18=false)
   var multiBank = req.query.multibank
@@ -198,7 +211,108 @@ router.get('/generic-truelayer/bank-success', function (req, res, next) {
                                               transactionsJsonIn = JSON.parse(body);
                                               console.log(transactionsJsonIn);
 
-                                              res.render('generic-truelayer/bank-success', {accounts: accountsJsonIn, balance: balanceJsonIn, transactions: transactionsJsonIn, });
+                                              res.render('generic-truelayer/bank-success', {accounts: accountsJsonIn, balance: balanceJsonIn, transactions: transactionsJsonIn});
+
+                                          } else {
+                                              console.log("There was an error: ") + response.statusCode;
+                                              console.log(body);
+                                          }
+                                    });
+
+                                } else {
+                                    console.log("There was an error: ") + response.statusCode;
+                                    console.log(body);
+                                }
+                          });
+
+                      } else {
+                          console.log("There was an error: ") + response.statusCode;
+                          console.log(body);
+                      }
+                  });
+
+          } else {
+              console.log("There was an error: ") + response.statusCode;
+              console.log(body);
+          }
+      });
+
+})
+
+
+
+router.get('/citizen-truelayer/bank-success', function (req, res, next) {
+
+  var domain = req.hostname;
+
+  if (domain=='localhost'){
+    var redirect_uri = 'http://' + domain + ':3000/citizen-truelayer/bank-success'
+  }
+  else{
+    var redirect_uri = 'https://laa-atp-express-evidence-proto.herokuapp.com/citizen-truelayer/bank-success'
+  }
+
+  console.log('>>>>>>>>>>>'+redirect_uri);
+
+  var authRequestUrl = 'https://auth.truelayer.com/connect/token';
+  var grant_type = "authorization_code";
+  var client_id = process.env.CLIENT_ID;
+  var client_secret = process.env.CLIENT_SECRET;
+  var code = req.query.code;
+
+  var authJsonOut = {grant_type: grant_type,
+              client_id: client_id,
+              client_secret: client_secret,
+              redirect_uri: redirect_uri,
+              code: code
+            }
+
+  console.log(authJsonOut);
+
+  request.post({
+      url: authRequestUrl,
+      headers: {'content-type':'application/json'},
+      form: authJsonOut
+    }, function (error, response, body) {
+
+          if (!error && response.statusCode == 200) {
+
+              authJsonIn = JSON.parse(body);
+              console.log(authJsonIn);
+
+              var token = authJsonIn.access_token;
+              var accountId = '8c0f6b05fa00f3f7142660c377237be1';
+              var getAccountsUrl = 'https://api.truelayer.com/data/v1/accounts';
+              var getBalanceUrl = getAccountsUrl + '/' + accountId + '/balance';
+              var getTransactionsUrl = getAccountsUrl + '/' + accountId + '/transactions';
+
+              var bearerToken = "Bearer " + token;
+
+              request.get({
+                  url: getAccountsUrl,
+                  headers: {Authorization: bearerToken}
+                }, function (error, response, body) {
+                      if (!error && response.statusCode == 200) {
+                          accountsJsonIn = JSON.parse(body);
+                          console.log(accountsJsonIn);
+
+                          request.get({
+                              url: getBalanceUrl,
+                              headers: {Authorization: bearerToken}
+                          }, function (error, response, body) {
+                                if (!error && response.statusCode == 200) {
+                                    balanceJsonIn = JSON.parse(body);
+                                    console.log(balanceJsonIn);
+
+                                    request.get({
+                                      url: getTransactionsUrl,
+                                      headers: {Authorization: bearerToken}
+                                    }, function (error, response, body) {
+                                          if (!error && response.statusCode == 200) {
+                                              transactionsJsonIn = JSON.parse(body);
+                                              console.log(transactionsJsonIn);
+
+                                              res.render('citizen-truelayer/bank-success', {accounts: accountsJsonIn, balance: balanceJsonIn, transactions: transactionsJsonIn});
 
                                           } else {
                                               console.log("There was an error: ") + response.statusCode;
